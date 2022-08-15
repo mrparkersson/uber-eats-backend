c1acfd2e617a578a09742f368ea053d21e8459d6
mrparkersson <amoparkerprince78@gmail.com>
2022-08-15 09:45:03 +0000

    Add UsersModule and CommonModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,5 @@
+import { User } from './users/entities/user.entity';
+
 import { Restaurant } from './restaurants/entities/restaurant.entity';
 import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
@@ -6,6 +8,8 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { UsersModule } from './users/users.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -26,7 +30,7 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       database: process.env.DB_DATABASE,
       synchronize: process.env.NODE_ENV !== 'prod',
       logging: /*process.env.NODE_ENV !== 'prod'*/ false,
-      entities: [Restaurant],
+      entities: [User],
     }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
@@ -35,6 +39,8 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       playground: true,
     }),
     RestaurantsModule,
+    UsersModule,
+    CommonModule,
   ],
 })
 export class AppModule {}
