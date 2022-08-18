7ea743926d1140c21a4c752fa54d9d72a639e63c
mrparkersson <amoparkerprince78@gmail.com>
2022-08-18 07:11:50 +0000

    Write tests for login

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -7,14 +7,14 @@ import { UserService } from './users.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-const mockRepository = {
+const mockRepository = () => ({
   findOne: jest.fn(),
   save: jest.fn(),
   create: jest.fn(),
-};
+});
 
 const mockJwtService = {
-  sign: jest.fn(),
+  sign: jest.fn(() => 'signed token'),
   verify: jest.fn(),
 };
 
@@ -27,17 +27,18 @@ type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 describe('UserService', () => {
   let service: UserService;
   let userRepository: MockRepository<User>;
-  beforeAll(async () => {
+  let jwtService: JwtService;
+  beforeEach(async () => {
     const module = await Test.createTestingModule({
       providers: [
         UserService,
         {
           provide: getRepositoryToken(User),
-          useValue: mockRepository,
+          useValue: mockRepository(),
         },
         {
           provide: getRepositoryToken(Verification),
-          useValue: mockRepository,
+          useValue: mockRepository(),
         },
         {
           provide: JwtService,
@@ -52,6 +53,7 @@ describe('UserService', () => {
 
     service = module.get<UserService>(UserService);
     userRepository = module.get(getRepositoryToken(User));
+    jwtService = module.get<JwtService>(JwtService);
   });
 
   it('should be defined', () => {
@@ -59,21 +61,71 @@ describe('UserService', () => {
   });
 
   describe('createAccount', () => {
+    const createAccountArgs = {
+      email: '',
+      password: '',
+      role: 0,
+    };
     it('should fail if user exists', async () => {
       userRepository.findOne.mockResolvedValue({
         id: 1,
         email: 'three@gmail.com',
       });
 
-      const result = await service.createAccount({
-        email: '',
-        password: '',
-        role: 0,
-      });
+      const result = await service.createAccount(createAccountArgs);
       expect(result).toMatchObject({
         ok: false,
         error: `User already exists`,
       });
+    });
+
+    it('should create a new user', async () => {
+      userRepository.findOne.mockResolvedValue(undefined);
+      userRepository.create.mockReturnValue(createAccountArgs);
+      await service.createAccount(createAccountArgs);
+      expect(userRepository.create).toHaveBeenCalledTimes(1);
+      expect(userRepository.create).toHaveBeenCalledWith(createAccountArgs);
+      expect(userRepository.save).toHaveBeenCalledTimes(1);
+      expect(userRepository.save).toHaveBeenCalledWith(createAccountArgs);
+    });
+
+    it('should fail on exception', async () => {
+      userRepository.findOne.mockRejectedValue(new Error());
+      const result = await service.createAccount(createAccountArgs);
+      expect(result).toEqual({ ok: false, error: `Couldn't create account` });
+    });
+  });
+
+  describe('login', () => {
+    const loginArgs = {
+      email: 'j@gmail.com',
+      password: 'j.password',
+    };
+    it('should fail if user does not exists', async () => {
+      userRepository.findOne.mockResolvedValue(null);
+      expect(userRepository.findOne).toHaveBeenCalledTimes(0);
+    });
+
+    it('should fail it the password is wrong', async () => {
+      const mockedUser = {
+        checkPassword: jest.fn(() => Promise.resolve(false)),
+      };
+
+      userRepository.findOne.mockResolvedValue(mockedUser);
+      const result = await service.login(loginArgs);
+      expect(result).toEqual({ ok: false, error: 'Incorrect password' });
+    });
+
+    it('should return token if the password is correct', async () => {
+      const mockedUser = {
+        id: 1,
+        checkPassword: jest.fn(() => Promise.resolve(true)),
+      };
+
+      userRepository.findOne.mockResolvedValue(mockedUser);
+      await service.login(loginArgs);
+      expect(jwtService.sign).toHaveBeenCalledTimes(1);
+      expect(jwtService.sign).toHaveBeenCalledWith({ id: 1 });
     });
   });
 
